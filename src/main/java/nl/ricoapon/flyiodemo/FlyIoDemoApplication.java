259906c832c12ee5a4c4a7423164dbfd14205c5b
Rico Apon <rico.apon@gmail.com>
2022-09-09 22:25:06 +0200

    Initial commit

---
+++ src/main/java/nl/ricoapon/flyiodemo/FlyIoDemoApplication.java
@@ -0,0 +1,13 @@
+package nl.ricoapon.flyiodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyIoDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlyIoDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/ricoapon/flyiodemo/FlyIoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.ricoapon.flyiodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyIoDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
