926c4fd46ed5d1ec756ec4b6c372e71fa604b957
Rico Apon <rico.apon@gmail.com>
2022-09-09 22:32:45 +0200

    Add Hello World controller for testing

---
+++ src/main/java/nl/ricoapon/flyiodemo/HelloWorldController.java
@@ -0,0 +1,12 @@
+package nl.ricoapon.flyiodemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello world";
+    }
+}
